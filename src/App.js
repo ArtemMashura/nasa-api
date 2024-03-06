b369e0135e22b724213810be0dd98490896076e1
ArtemMashura <Masu_ih91@student.itstep.org>
2024-03-06 15:30:25 +0200

    pixabay

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [data, setData] = useState([])
+  const ref = useRef(null)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={ref} id='name'></input>
+      <button onClick={() => {
+        var search = ref.current.value
+        axios.get(`https://pixabay.com/api/?key=42728892-9cc4f7d0b5fde0edf075cd096&q=${search}&image_type=photo`)
+        .then((res) => {
+          console.log(res.data.hits)
+          setData(res.data.hits);
+        })
+      }}>Search</button>
+      <div>
+        {data &&
+          data.map((image, index) => (
+            <img key={index} src={image.previewURL} alt='' width={100} />
+          ))}
+      </div>
     </div>
   );
 }
